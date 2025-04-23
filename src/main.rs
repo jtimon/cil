cf5443393adcb1288b11f9a09897052de33bf6d1
Jorge Timón <jtimon@jtimon.cc>
2025-04-24 01:50:30 +0200

    how would it take to fix the build? too much

--- src/main.rs
+++ src/main.rs
@@ -3899,7 +3899,14 @@ fn run_file(path: &String, main_args: Vec<String>) -> Result<(), String> {
     let mut context = Context::new(DEFAULT_MODE);
     run_file_with_context(true, &mut context, &"src/core/core.cil".to_string(), Vec::new())?;
     run_file_with_context(true, &mut context, &"src/core/std.cil".to_string(), Vec::new())?;
-    run_file_with_context(true, &mut context, &"src/core/test.cil".to_string(), Vec::new())?;
+    println!("run_file: mode: '{}', is test? '{}'", context.mode.name, context.mode.name == "test");
+
+    println!("AAAAAAA: not test name: '{}'", context.mode.name);
+    if context.mode.name == "test" {
+        run_file_with_context(true, &mut context, &"src/core/modes/test.cil".to_string(), Vec::new())?;
+    } else {
+        println!("not test name: '{}'", context.mode.name)
+    }
     run_file_with_context(false, &mut context, &path, main_args)?;
     return Ok(())
 }
